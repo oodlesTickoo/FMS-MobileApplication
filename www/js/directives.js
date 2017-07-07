378e6e08d2e7c49c72cf7138172ddd21668495c0
Milind Ahuja <milind.ahuja@oodlestechnologies.com>
2017-07-07 16:39:58 +0530

    resolved ionic accordion issue on faq age

--- www/js/directives.js
+++ www/js/directives.js
@@ -11,3 +11,18 @@ angular.module('starter.directives', [])
     }
   };
 })
+
+.directive('ionMdInput', function(){
+  return {
+    restrict: 'E',
+    transclude: true,
+    template:
+      '<input type="text" required>'+
+      '<span class="md-highlight"></span>'+
+      '<span class="md-bar"></span>'+
+      '<label>{{label}}</label>',
+    scope: {
+      'label': '@'
+    }
+  }
+});
